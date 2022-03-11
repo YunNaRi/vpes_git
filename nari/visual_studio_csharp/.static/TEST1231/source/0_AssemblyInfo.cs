685d02234d5a332e3d9232ef6d0f08afa913e6f3
nryun <nryun@suresofttech.com>
2022-03-11 10:49:56 +0900

    추가

---
+++ nari/visual_studio_csharp/.static/TEST1231/source/0_AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("STATIC_CSHARP_EXAMPLE")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("STATIC_CSHARP_EXAMPLE")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("411dcd57-e94e-437c-9aad-c8828fc0f12b")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
