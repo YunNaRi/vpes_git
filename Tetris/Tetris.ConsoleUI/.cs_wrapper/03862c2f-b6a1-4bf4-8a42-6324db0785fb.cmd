bb88719aa1461b8ff9b4b1409c0d58d839fa4c6b
nryun <nryun@suresofttech.com>
2023-04-17 16:20:36 +0900

    add

---
+++ Tetris/Tetris.ConsoleUI/.cs_wrapper/03862c2f-b6a1-4bf4-8a42-6324db0785fb.cmd
@@ -0,0 +1,5 @@
+if defined CODESCROLL_TM_INSTALL_PATH (
+	"%CODESCROLL_TM_INSTALL_PATH%\csc\QualityScroll.Cover.CscHooker.exe" "C:\Users\ysgo\Desktop\VPES\VPES2.3.2\svn\VPES_Study\COVER_EE\Tetris\Tetris.ConsoleUI\Tetris.ConsoleUI.csproj" %*
+) else (
+	echo Error : please SET "CODESCROLL_TM_INSTALL_PATH"
+)
---
+++ Tetris/test.bat
@@ -0,0 +1,6 @@
+echo curl
+for /l %%i in (1, 1, 200) do (
+echo %%i/200
+echo %DATE% %TIME%
+static-csharp -k CS1 -s 211.116.223.195 -i C:\Users\ysgo\Desktop\STATIC-C#\Tetris\Tetris.sln
+)
