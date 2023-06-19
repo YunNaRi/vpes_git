da7775c0a95e9e241cd202f4e7068a261fb809ef
nryun <nryun@suresofttech.com>
2023-06-20 08:15:49 +0900

    add

--- miro_vs2015/.staticdata/capture.bat
+++ miro_vs2015/.staticdata/capture.bat
@@ -1,5 +1,5 @@
 @echo off
 chcp 65001
-cd "D:\VPES_GIT\vpes_git\miro_vs2015"
+cd "C:\Program Files\Suresoft\QualityScroll VPES 3.2.2\vpes\STATIC_WORKSPACE\Auto_test_static_c\miro_vs2015"
 "msbuild" "vs2015_miro.sln" "/t:rebuild"
 chcp 949 >nul
